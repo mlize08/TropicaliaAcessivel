70283c4f34fac47a20e8c3313d2214bdd8a59e2d
Monalize M <150396638+mlize08@users.noreply.github.com>
2025-04-06 19:22:09 +0000

    s

--- script.js
+++ script.js
@@ -20,5 +20,10 @@ botaoDeAcessibilidade.addEventListener('click', function(){
     botaoDeAcessibilidade.classList.toggle('rotacao-botao');
     opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
 })
+const alternaContraste = document.getElementById('alterna-contraste')
+
+alternaContraste.addEventListener('click', function () {
+    document.body.classList.toggle('alto-contraste')
+})
 
 })
